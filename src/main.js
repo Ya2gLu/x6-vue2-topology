f47b55b2956b044a54e0e4e3728b56daca4df6eb
ya2glu <ya2glu@163.com>
2023-06-06 09:16:57 +0800

    remove pinia

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 
 import App from './App.vue'
 import router from './router'
-import { createPinia, PiniaVuePlugin } from 'pinia'
 
 import { Tooltip } from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
@@ -10,7 +9,6 @@ import 'ant-design-vue/dist/antd.css'
 import './assets/main.css'
 
 Vue.config.productionTip = false
-Vue.use(PiniaVuePlugin)
 Vue.use(Tooltip)
 
 const pinia = createPinia()
